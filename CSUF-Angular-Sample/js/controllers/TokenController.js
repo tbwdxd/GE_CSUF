16f99acc968a3c7cf7555beba755171ab97f5237
root <root@localhost.localdomain>
2016-03-20 15:47:31 -0700

    Hopeless Attempting

---
+++ CSUF-Angular-Sample/js/controllers/TokenController.js
@@ -0,0 +1,13 @@
+myApp.controller('TokenController', ['$scope', 'gettokenservice', function($scope, gettokenservice) {
+    $scope.token = null;
+    
+    // If the catalogservice is successful in getting data display it
+    gettokenservice.success(function(dataResponse) {
+        $scope.token = dataResponse;
+    });
+    // If the catalogservice is failed then return error message
+    gettokenservice.error(function(dataResponse) {
+        $scope.token = dataResponse;
+    });
+}]);
+
---
+++ CSUF-Angular-Sample/js/services/get_token.js
@@ -0,0 +1,20 @@
+// I use factory I think its the same as service though
+myApp.factory('gettokenservice', ['$http',function($http) {
+    delete $http.defaults.headers.common['X-Requested-With'];
+    return $http({
+        method: 'POST',
+        url: 'https://bc026551-76ad-49c9-a2b5-664e5cc6e1c0.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token',
+        headers: {'Authorization': 'Basic YWRtaW46PG15YWRtaW5zZWNyZXQ=',
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                  'x-tenant': 'bc026551-76ad-49c9-a2b5-664e5cc6e1c0'
+                 },
+        data: {data: 'client_id=admin&grant_type=client_credentials&client_secret=myadminsecret'}
+        })
+    .success(function(response) {
+        return response;
+    })
+    .error(function(err) {
+        return err;
+    })
+        
+    }]);
