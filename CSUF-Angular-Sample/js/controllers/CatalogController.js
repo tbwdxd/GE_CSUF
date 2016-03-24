391a9fb54f9180ab17f35e0f6f0df9b1a4ed1a83
MapleSun <maplesyj@gmail.com>
2016-03-23 18:03:11 -0700

    Use TokenController to control two services, cannot pass $scope variable

--- CSUF-Angular-Sample/js/controllers/CatalogController.js
+++ CSUF-Angular-Sample/js/controllers/CatalogController.js
@@ -4,10 +4,12 @@
 // It also allows the $scope to be used within all files as well
 myApp.controller('CatalogController', ['$scope', 'catalogservice', function($scope, catalogservice) {
     $scope.catalogData = null;
-    
+    	//console.log($scope.token);
+ 	console.log(20);
     // If the catalogservice is successful in getting data display it
     catalogservice.success(function(dataResponse) {
         $scope.catalogData = dataResponse;
+        console.log($scope.token);
     });
     // If the catalogservice is failed then return error message
     catalogservice.error(function(errorResponse) {
--- CSUF-Angular-Sample/js/controllers/TokenController.js
+++ CSUF-Angular-Sample/js/controllers/TokenController.js
@@ -1,13 +1,30 @@
-myApp.controller('TokenController', ['$scope', 'gettokenservice', function($scope, gettokenservice) {
+myApp.controller('TokenController', ['$scope', 'gettokenservice', 'catalogservice' ,function($scope, gettokenservice, catalogservice) {
     $scope.token = null;
     
     // If the catalogservice is successful in getting data display it
     gettokenservice.success(function(dataResponse) {
         $scope.token = dataResponse;
+        //shareScope.set(dataResponse);
+        console.log(dataResponse);
+
+        //catalogservice
+        $scope.catalogData = null;
+        catalogservice.success(function(dataResponse) {
+       		$scope.catalogData = dataResponse;
+        //console.log($scope.token);
+    	});
+    // If the catalogservice is failed then return error message
+    	catalogservice.error(function(errorResponse) {
+        	$scope.catalogData = errorResponse;
+    	});
     });
     // If the catalogservice is failed then return error message
     gettokenservice.error(function(dataResponse) {
         $scope.token = dataResponse;
     });
+    
+
+    // If the catalogservice is successful in getting data display it
+    
 }]);
 
