4fca8e3bf444a4c82b9180941b496c6c68f39f33
Brandi Frisbie <brandi.frisbie@ge.com>
2016-03-24 15:51:36 -0700

    initial commit

---
+++ public/scripts/routes.js
@@ -0,0 +1,32 @@
+/**
+ * Router Config
+ * This is the router definition that defines all application routes.
+ */
+define(['angular', 'angular-ui-router'], function(angular) {
+    'use strict';
+    return angular.module('app.routes', ['ui.router']).config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+        //Turn on or off HTML5 mode which uses the # hash
+        $locationProvider.html5Mode(true).hashPrefix('!');
+
+        /**
+         * Router paths
+         * This is where the name of the route is matched to the controller and view template.
+         */
+        $stateProvider
+            .state('dashboards', {
+                url: '/dashboards',
+                templateUrl: 'views/dashboards.html',
+                controller: 'DashboardsCtrl'
+            });
+
+
+
+        $urlRouterProvider.otherwise(function ($injector) {
+            var $state = $injector.get('$state');
+            document.querySelector('px-app-nav').markSelected('/dashboards');
+            $state.go('dashboards');
+        });
+
+    }]);
+});
