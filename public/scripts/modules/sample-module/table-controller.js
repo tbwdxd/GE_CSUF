9b0d0b79cfba7f469cef271a4db212b814266d73
Luis <luis.covarrubias1@gmail.com>
2016-04-25 00:28:02 -0700

    Files to test timeseries respsone offline

--- public/scripts/modules/sample-module/table-controller.js
+++ public/scripts/modules/sample-module/table-controller.js
@@ -1,10 +1,11 @@
-define(['angular', './sample-module'], function (angular, controllers) {
+define(['angular', './sample-module'], function (ngular, controllers) {
     'use strict';
 
     // Controller definition
     controllers.controller('TableCtrl', ['$scope', '$http', function($scope, $http) {
     		
     		$scope.price = 0.12;
+    		$scope.solarData = null;
     		
     		// Get the monthly data
         	$http.get('../sample-data/avg.json')
@@ -18,7 +19,35 @@ define(['angular', './sample-module'], function (angular, controllers) {
             $scope.downTime = res.data;
         });
         	
+        	$http.get('../sample-data/testData.json')
+        	.then(function(predixTimeSeriesData) {
+			   $scope.solarData = data.tags[0].results[0].values.map(
+       		 function(curVal, index, arr) {
+            	return [curVal[0], curVal[1]];
+        		}
+    			);
 
-   
+    			conosle.log($scope.solarData);
+    	});
+    	
+    	
+        	/*
+        $http({
+           url: '/api/v1/datapoints',
+           method: 'POST',    
+           data: '{"start":"1y-ago","tags":[{"name":"SolarData"}]}'
+         }).then(function(data, status){
+		var requirementArray = new Array();
+		for (var i = 0; i < $scope.solarData.length ; i++) {
+    			var pare = new Array();
+    			pare.push($scope.solarData[i][0]);
+    			pare.push($scope.solarData[i][1]);
+
+    			requirementArray.push(pare);
+		}
+		$scope.solarData = requirmentArray;
+		console.log($scope.solarData);
+    	});
+    	*/
     }]);
 });
