6b6c3431ac0b0aa9478c187efba1ab186e6b0501
Luis <luis.covarrubias1@gmail.com>
2016-04-25 01:38:06 -0700

    time series query to chart working

--- public/scripts/modules/sample-module/table-controller.js
+++ public/scripts/modules/sample-module/table-controller.js
@@ -19,9 +19,10 @@ define(['angular', './sample-module'], function (ngular, controllers) {
             $scope.downTime = res.data;
         });
         	
+        	/*
         	$http.get('../sample-data/testData.json')
         	.then(function(predixTimeSeriesData) {
-			   $scope.solarData = data.tags[0].results[0].values.map(
+			   $scope.solarData = predixTimeSeriesData.data.tags[0].results[0].values.map(
        		 function(curVal, index, arr) {
             	return [curVal[0], curVal[1]];
         		}
@@ -29,25 +30,21 @@ define(['angular', './sample-module'], function (ngular, controllers) {
 
     			conosle.log($scope.solarData);
     	});
+    	*/
     	
     	
-        	/*
         $http({
            url: '/api/v1/datapoints',
            method: 'POST',    
            data: '{"start":"1y-ago","tags":[{"name":"SolarData"}]}'
-         }).then(function(data, status){
-		var requirementArray = new Array();
-		for (var i = 0; i < $scope.solarData.length ; i++) {
-    			var pare = new Array();
-    			pare.push($scope.solarData[i][0]);
-    			pare.push($scope.solarData[i][1]);
-
-    			requirementArray.push(pare);
-		}
-		$scope.solarData = requirmentArray;
+         }).then(function(predixTimeSeriesData){
+				 $scope.solarData = predixTimeSeriesData.data.tags[0].results[0].values.map(
+       		 function(curVal, index, arr) {
+            	return [curVal[0], curVal[1]];
+        		}
+    			);
 		console.log($scope.solarData);
     	});
-    	*/
+    	
     }]);
 });
