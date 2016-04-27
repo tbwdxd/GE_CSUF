9711d8b76a5c038a1206f20e3b8671c1c61d7b71
porterj091 <joporter5606@live.com>
2016-04-27 15:51:08 -0700

    Trigen Schedule

--- public/scripts/modules/sample-module/TrigenController.js
+++ public/scripts/modules/sample-module/TrigenController.js
@@ -5,6 +5,17 @@ define(['angular', './sample-module'], function (angular, controllers) {
     	
     		$scope.price = 0.135;
     		$scope.monthsData = null;
+    		$scope.dayBlocks = null;
+    		$scope.month = null;
+    		
+    		// Days in the week
+    		$scope.monday = null;
+    		$scope.tuesday = null;
+    		$scope.wednesday = null;
+    		$scope.thursday = null;
+    		$scope.friday = null;
+    		$scope.saturday = null;
+    		$scope.sunday = null;
     	
     		// Get the Trigen DownTime
          $http.get('../sample-data/trigenDownTime.json')
@@ -19,15 +30,50 @@ define(['angular', './sample-module'], function (angular, controllers) {
         });
         
         
-        $scope.update = function(Month) {
+        $scope.update = function(Month, Hour) {
         	
         		angular.forEach($scope.trigenSchedule, function(value, index){
         		
         			if (value.name == Month) {
 	        			$scope.monthsData = value;
+	        			$scope.month = Month;
 	        		}
         	
         	
+        		});
+        		
+        		$scope.daysBlocks = [];
+        		angular.forEach($scope.monthsData.days, function(value, index) {
+        				var tempBlock = [];
+        				if (value.name == "Monday") {
+        					console.log("Monday");
+        					for (i = 0 < value.bestStarts.length())	
+        					{
+        						if (value.bestEnd[i] == [])
+        						{
+        							tempBlock.append(value.bestStarts[i] + '- End of day');
+        						}
+        					}
+        				}
+        				else if (value.name = "Tuesday") {
+        					console.log("Tuesday");
+        				}
+        				else if (value.name = "Wednesday") {
+        					console.log("Wednesday");
+        				}
+        				else if (value.name = "Thursday") {
+        					console.log("Thursday");
+        				}
+        				else if (value.name = "Friday") {
+        					console.log("Friday");
+        				}
+        				else if (value.name = "Saturday") {
+        					console.log("Saturday");
+        				}
+        				else if (value.name = "Sunday") {
+        					console.log("Sunday");
+        				}
+
         		});
         	
         		
