a868b15b98a0bccd5ab59bbac71b932c38b2d0a5
Bowen Tian <taylortian@csu.fullerton.edu>
2016-04-27 22:46:54 -0700

    fixed bug in main.js

--- public/scripts/modules/sample-module/main.js
+++ public/scripts/modules/sample-module/main.js
@@ -1,4 +1,4 @@
-define(['./sample-module', './sample-directive', './sample-filter', './sample-service', './dashboard-controller', './HomeController','./realtimeController',
+define(['./sample-module', './sample-directive', './sample-filter', './sample-service', './dashboard-controller', './HomeController',
     './sample-controller', './table-controller', './predix-asset-service', './predix-user-service', './predix-view-service', './TrigenController'], function() {
 
 });
