32cfd707f762bc5f76344bd2f0173937540f7d6f
Bowen Tian <taylortian@csu.fullerton.edu>
2016-04-28 18:45:04 -0700

    need enable the tab

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -40,14 +40,12 @@ define(['angular', 'angular-ui-router'], function(angular) {
                 templateUrl: 'views/dashboards.html',
                 controller: 'DashboardsCtrl'
             })
-		    /*.state('real-time', {
+		    .state('real-time', {
             	parent: 'secure',
                 url: '/real-time',
                 templateUrl: 'views/real-time.html',
-                controller: 'TrigenCtrl'
-            })
                 controller: 'DashboardsCtrl'
-            })*/
+            })
 		    .state('total', {
             	parent: 'secure',
                 url: '/total',
